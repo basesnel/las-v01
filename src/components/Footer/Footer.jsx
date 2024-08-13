dcb92bc4a294c7bbc1002967b7a5fe98431178bd
basesnel <basesnel@gmail.com>
2024-08-13 14:31:15 +0300

    Applayed fit content in Header and Footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,19 @@
 import formatTimeAgo from "../../helpers/formatTimeAgo";
-import Section from "../Section/Section";
+import Container from "../Container/Container";
 
 import styles from "./styles.module.css";
 
 const Footer = () => {
   return (
     <footer>
-      <Section>
+      <Container>
         <h2 className={styles.title}>Auto-assistant</h2>
         <p className={styles.date}>
           {`created ${formatTimeAgo(
             "Mon Aug 12 2024 16:50:00 GMT+0300 (Eastern European Summer Time)"
           )}`}
         </p>
-      </Section>
+      </Container>
     </footer>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
 import formatDate from "../../helpers/formatDate";
-import Section from "../Section/Section";
+import Container from "../Container/Container";
 
 import styles from "./styles.module.css";
 
 const Header = () => {
   return (
     <header>
-      <Section>
+      <Container>
         <h1 className={styles.title}>lashenko auto-service</h1>
         <p className={styles.date}>Auto-tool | {formatDate(new Date())}</p>
-      </Section>
+      </Container>
     </header>
   );
 };
