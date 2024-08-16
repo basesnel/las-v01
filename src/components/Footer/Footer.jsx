e022ff7e67c7e309b46729c514d2f896ef918cda
basesnel <basesnel@gmail.com>
2024-08-16 11:19:23 +0300

    Added markup of social nets to Footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,23 @@
 import Container from "../Container/Container";
-import Heading from "../Heading/Heading";
 import formatTimeAgo from "../../helpers/formatTimeAgo";
 
 import styles from "./styles.module.css";
+import socNets from "../../constants/socials";
 
 const Footer = () => {
   return (
     <footer>
       <Container>
-        <Heading level={2} title="auto-assistant" />
+        <ul>
+          {socNets.map(({ id, text, src }) => {
+            return (
+              <li key={id}>
+                <a href={src}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+        <p className={styles.copyrigh}>Copyright 2024</p>
         <p className={styles.date}>
           {`created ${formatTimeAgo(
             "Mon Aug 12 2024 16:50:00 GMT+0300 (Eastern European Summer Time)"
---
+++ src/constants/socials.js
@@ -0,0 +1,14 @@
+const socNets = [
+  {
+    id: 1,
+    text: "Instagram",
+    src: "https://www.instagram.com/liashenko_auto_service",
+  },
+  {
+    id: 2,
+    text: "facebook",
+    src: "https://www.facebook.com/profile.php?id=61554802258333",
+  },
+];
+
+export default socNets;
