fbac5536aad151da7a5828abbcd95af38a855478
basesnel <basesnel@gmail.com>
2024-08-20 15:06:54 +0300

    Added list of benefits to Benefits-section.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,6 +1,7 @@
 import Section from "../Section/Section";
 import Heading from "../Heading/Heading";
 import Subtitle from "../Subtitle/Subtitle";
+import benefits from "../../constants/benefits";
 
 import styles from "./styles.module.css";
 
@@ -9,7 +10,18 @@ const Features = () => {
     <Section part="features" label="our benefits and features">
       <Subtitle>why people come to us?</Subtitle>
       <Heading level={2} title="our benefits" />
-      <p>List of benefits.</p>
+      <ul className={styles.list}>
+        {benefits.map(({ id, amount, text }) => {
+          return (
+            <li key={id} className={styles.item}>
+              <div className={styles.benefit}>
+                <p>{amount}</p>
+                <p>{text}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </Section>
   );
 };
---
+++ src/constants/benefits.js
@@ -0,0 +1,24 @@
+const benefits = [
+  {
+    id: 0,
+    amount: 200,
+    text: "Satisfied customers per day.",
+  },
+  {
+    id: 1,
+    amount: 40,
+    text: "Number of services",
+  },
+  {
+    id: 2,
+    amount: 30,
+    text: "Poltava's best masters",
+  },
+  {
+    id: 3,
+    amount: 100,
+    text: "Promotional discounts.",
+  },
+];
+
+export default benefits;
