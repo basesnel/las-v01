26fa9f84a5d43a44361343abdf49e51ebaeba88a
basesnel <basesnel@gmail.com>
2024-08-21 16:51:36 +0300

    Styled gallery-section.

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,11 +5,21 @@ import Subtitle from "../Subtitle/Subtitle";
 import styles from "./styles.module.css";
 
 const Gallery = () => {
+  const images = ["image", "image", "image", "image"];
+
   return (
     <Section part="gallery" label="our work">
       <Subtitle>complex car repair</Subtitle>
-      <Heading level={2} title="gallery" />
-      <p className={styles.date}>List of pictures.</p>
+      <Heading level={2} mode="gallery" title="gallery" />
+      <ul>
+        {images.map((image, i) => (
+          <li key={i} className={styles.item}>
+            <div className={styles.thumb}>
+              <p className={styles.inner}>{`${image} ${i}`}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </Section>
   );
 };
