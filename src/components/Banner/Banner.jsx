986b25e1d779d9e52366cd5e10badb5c34922614
basesnel <basesnel@gmail.com>
2024-09-05 20:46:19 +0300

    Created block of content in the Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,23 +5,20 @@ import Heading from "../Heading/Heading";
 import Text from "../Text/Text";
 import Data from "../Data/Data";
 
-// import styles from "./styles.module.css";
+import styles from "./styles.module.css";
 
 const Banner = () => {
   return (
-    <Section
-      part="banner"
-      variant="dark"
-      mode="banner"
-      label="banner with news"
-    >
-      <Subtitle>a quality car service in poltava</Subtitle>
-      <Heading level={1} title="liashenko auto service" />
-      <Text mode="banner">
-        Services for cars. We invite you to visit LAS, where you can get a full
-        range of quality services, namely:
-      </Text>
-      <Data mode="banner">Auto-tool | {formatDate(new Date())}</Data>
+    <Section part="banner" mode="banner" label="banner with news">
+      <div className={styles.content}>
+        <Subtitle>a quality car service in poltava</Subtitle>
+        <Heading level={1} title="liashenko auto service" />
+        <Text mode="banner">
+          Services for cars. We invite you to visit LAS, where you can get a
+          full range of quality services, namely:
+        </Text>
+        <Data mode="banner">Auto-tool | {formatDate(new Date())}</Data>
+      </div>
     </Section>
   );
 };
