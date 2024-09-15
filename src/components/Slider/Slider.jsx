1bbec2a8b9fd468cefd57c4e2027476bcedbb65b
basesnel <basesnel@gmail.com>
2024-09-15 17:24:44 +0300

    Apply array renders in Slider component.

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,81 +1,55 @@
+import { useState } from "react";
 import SLIDES from "../../constants/slides";
 import styles from "./styles.module.css";
 
-const Slider = ({ variant }) => {
+const Slider = () => {
+  const [index] = useState(0);
+
+  const dots = [];
+
+  for (let i = 0; i < SLIDES.length; i++) {
+    dots.push(
+      <div
+        key={i}
+        className={i === index ? `${styles.dot} ${styles.active}` : styles.dot}
+      ></div>
+    );
+  }
+
   return (
     <div className={styles.slider}>
       <div className={styles.wrapper}>
         <div className={styles.line}>
-          <div className={styles.slide}>
-            <picture>
-              <source
-                srcSet={SLIDES[0].mobWEBP1}
-                type="image/webp"
-                width={480}
-                height={580}
-              />
+          {SLIDES.map((SLIDE, i) => {
+            return (
+              <div key={i} className={styles.slide}>
+                <picture>
+                  <source
+                    srcSet={SLIDE.mobWEBP1}
+                    type="image/webp"
+                    width={480}
+                    height={580}
+                  />
 
-              <img
-                loading="lazy"
-                src={SLIDES[0].mobJPG1}
-                alt={SLIDES[0].alt}
-                width={480}
-                height={580}
-                className={styles.image}
-              />
-            </picture>
-          </div>
-
-          <div className={styles.slide}>
-            <picture>
-              <source
-                srcSet={SLIDES[1].mobWEBP2}
-                type="image/webp"
-                width={480}
-                height={580}
-              />
-
-              <img
-                loading="lazy"
-                src={SLIDES[1].mobJPG2}
-                alt={SLIDES[1].alt}
-                width={480}
-                height={580}
-                className={styles.image}
-              />
-            </picture>
-          </div>
-
-          <div className={styles.slide}>
-            <picture>
-              <source
-                srcSet={SLIDES[2].mobWEBP3}
-                type="image/webp"
-                width={480}
-                height={580}
-              />
-
-              <img
-                loading="lazy"
-                src={SLIDES[2].mobJPG3}
-                alt={SLIDES[2].alt}
-                width={480}
-                height={580}
-                className={styles.image}
-              />
-            </picture>
-          </div>
+                  <img
+                    loading="lazy"
+                    src={SLIDE.mobJPG1}
+                    alt={SLIDE.alt}
+                    width={480}
+                    height={580}
+                    className={styles.image}
+                  />
+                </picture>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className={styles.buttons}>
         <button className={styles.prev}>prev</button>
         <button className={styles.next}>next</button>
       </div>
-      <div className={styles.dots}>
-        <div className={`${styles.dot} ${styles.active}`}></div>
-        <div className={styles.dot}></div>
-        <div className={styles.dot}></div>
-      </div>
+      <div className={styles.dots}>{dots}</div>
     </div>
   );
 };
