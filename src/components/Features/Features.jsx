ed8cec1072c44eca3aaea1bc1fa859bd3c30d854
basesnel <basesnel@gmail.com>
2024-09-24 18:38:37 +0300

    Prepared content for Features-section.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,17 +1,38 @@
 import Section from "../Section/Section";
 import Heading from "../Heading/Heading";
 import Subtitle from "../Subtitle/Subtitle";
-import benefits from "../../constants/benefits";
+import Text from "../Text/Text";
+import features from "../../constants/features";
 
 import styles from "./styles.module.css";
 
 const Features = () => {
+  const {
+    subTitle,
+    title,
+    preText,
+    firstText,
+    secondText,
+    thirdText,
+    list,
+    stats,
+  } = features;
+
   return (
     <Section indexSection={3}>
-      <Subtitle>why people come to us?</Subtitle>
-      <Heading level={2} title="our benefits" />
+      <Subtitle>{subTitle.uk}</Subtitle>
+      <Heading level={2} title={title.uk} />
+      <Text mode="introWelcome">{preText.uk}</Text>
+      <Text mode="intro">{firstText.uk}</Text>
+      <Text mode="intro">{secondText.uk}</Text>
+      <ul className="">
+        {list.map(({ uk }, i) => (
+          <li key={i}>{uk}</li>
+        ))}
+      </ul>
+      <Text mode="intro">{thirdText.uk}</Text>
       <ul className={styles.list}>
-        {benefits.map(({ amount, text }, i) => {
+        {stats.map(({ amount, text }, i) => {
           return (
             <li key={i} className={styles.item}>
               <div className={styles.benefit}>
---
+++ src/constants/features.js
@@ -0,0 +1,72 @@
+const features = {
+  subTitle: {
+    en: "why people come to us?",
+    uk: "Чому люди приходять до нас?",
+  },
+  title: {
+    en: "iour benefits",
+    uk: "наші переваги",
+  },
+  preText: {
+    en: "What's LAS?",
+    uk: "Що таке LAS?",
+  },
+  firstText: {
+    en: "We invite you to visit our LAS car service center. What awaits you:",
+    uk: "Запрошуємо Вас відвідати наш автосервіс LAS. На Вас чекає:",
+  },
+  secondText: {
+    en: "We carry out work of any complexity, with high quality and with a guarantee.",
+    uk: "Виконуємо роботи будь-якої складності, якісно та з гарантією.",
+  },
+  thirdText: {
+    en: "Your trust is the most important thing for us!",
+    uk: "Найголовніше для нас - Ваша довіра!",
+  },
+  list: [
+    {
+      en: "High-quality maintenance",
+      uk: "Якісне технічне обслуговування",
+    },
+    {
+      en: "Highly qualified repair work",
+      uk: "Висококваліфіковані ремонтні роботи",
+    },
+    {
+      en: "Experienced craftsmen",
+      uk: "Досвідчені майстри",
+    },
+    {
+      en: "High-quality and proven spare parts",
+      uk: "Якісні та перевірені запчастини",
+    },
+    {
+      en: "Professional selection of spare parts",
+      uk: "Професіональний підбір запчастин",
+    },
+    {
+      en: "Personal control of service quality",
+      uk: "Особистий контроль якості обслуговування",
+    },
+  ],
+  stats: [
+    {
+      amount: 200,
+      text: "Satisfied customers per day.",
+    },
+    {
+      amount: 40,
+      text: "Number of quality services",
+    },
+    {
+      amount: 30,
+      text: "Poltava's best masters",
+    },
+    {
+      amount: 100,
+      text: "Promotional discounts.",
+    },
+  ],
+};
+
+export default features;
