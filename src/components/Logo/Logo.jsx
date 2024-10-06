7da0da7ee61db98f118ea9bbb05aad156d14baa7
basesnel <basesnel@gmail.com>
2024-10-06 19:20:23 +0300

    Reallocation images of logo.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,36 +1,24 @@
-import Logoltwebp from "../../assets/logo/logo-light-130.webp";
-import Logoltpng from "../../assets/logo/logo-light-130.png";
-import Logodkwebp from "../../assets/logo/logo-dark-130.webp";
-import Logodkpng from "../../assets/logo/logo-dark-130.png";
 import logo from "../../constants/logo";
 
 import styles from "./styles.module.css";
 
 const Logo = ({ isDark, variant }) => {
-  const { label } = logo;
+  const { label, liteWBP, litePNG, darkWBP, darkPNG } = logo;
 
   const variation = variant ? `${styles[variant]}` : "";
 
   return (
     <a href="#" className={`${styles.logo} ${variation}`} aria-label={label.uk}>
       <picture focusable="false" aria-hidden="true">
-        <source
-          srcSet={Logodkwebp}
-          media="(min-width: 768px)"
-          type="image/webp"
-        />
+        <source srcSet={darkWBP} media="(min-width: 768px)" type="image/webp" />
 
-        <source
-          srcSet={Logodkpng}
-          media="(min-width: 768px)"
-          type="image/png"
-        />
+        <source srcSet={darkPNG} media="(min-width: 768px)" type="image/png" />
 
-        <source srcSet={isDark ? Logodkwebp : Logoltwebp} type="image/webp" />
+        <source srcSet={isDark ? darkWBP : liteWBP} type="image/webp" />
 
         <img
-          srcSet={isDark ? Logodkpng : Logoltpng}
-          src={isDark ? Logodkpng : Logoltpng}
+          srcSet={isDark ? darkPNG : litePNG}
+          src={isDark ? darkPNG : litePNG}
           alt="logo"
           width={100}
           height={100}
--- src/constants/logo.js
+++ src/constants/logo.js
@@ -1,8 +1,17 @@
+import LogoLiteWBP from "../assets/logo/logo-light-130.webp";
+import LogoLitePNG from "../assets/logo/logo-light-130.png";
+import LogoDarkWBP from "../assets/logo/logo-dark-130.webp";
+import LogoDarkPNG from "../assets/logo/logo-dark-130.png";
+
 const logo = {
   label: {
     en: "LAS logo",
     uk: "Логотип LAS",
   },
+  liteWBP: LogoLiteWBP,
+  litePNG: LogoLitePNG,
+  darkWBP: LogoDarkWBP,
+  darkPNG: LogoDarkPNG,
 };
 
 export default logo;
