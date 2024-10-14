587f1c8b5405aee5d335d632d7e97b0d88805120
basesnel <basesnel@gmail.com>
2024-10-14 19:40:10 +0300

    Added gallery object with data of Gallery-section.

---
+++ src/constants/gallery.js
@@ -0,0 +1,59 @@
+import mob1s0418jpg from "../assets/images/banner/mobile/slide-img-1-418.jpg";
+import mob1s0418wpp from "../assets/images/banner/mobile/slide-img-1-418.webp";
+import mob1s0836jpg from "../assets/images/banner/mobile/slide-img-1-836.jpg";
+import mob1s0836wpp from "../assets/images/banner/mobile/slide-img-1-836.webp";
+import mob1s1254jpg from "../assets/images/banner/mobile/slide-img-1-1254.jpg";
+import mob1s1254wpp from "../assets/images/banner/mobile/slide-img-1-1254.webp";
+import mob1s1672jpg from "../assets/images/banner/mobile/slide-img-1-1672.jpg";
+import mob1s1672wpp from "../assets/images/banner/mobile/slide-img-1-1672.webp";
+
+// import tab1s0570jpg from "../assets/images/banner/tablet/slide-img-1-570.jpg";
+// import tab1s0570wpp from "../assets/images/banner/tablet/slide-img-1-570.webp";
+// import tab1s1140jpg from "../assets/images/banner/tablet/slide-img-1-1140.jpg";
+// import tab1s1140wpp from "../assets/images/banner/tablet/slide-img-1-1140.webp";
+// import tab1s1710jpg from "../assets/images/banner/tablet/slide-img-1-1710.jpg";
+// import tab1s1710wpp from "../assets/images/banner/tablet/slide-img-1-1710.webp";
+// import tab1s2280jpg from "../assets/images/banner/tablet/slide-img-1-2280.jpg";
+// import tab1s2280wpp from "../assets/images/banner/tablet/slide-img-1-2280.webp";
+
+// import dtp1s1040jpg from "../assets/images/banner/desktop/slide-img-1-1040.jpg";
+// import dtp1s1040wpp from "../assets/images/banner/desktop/slide-img-1-1040.webp";
+// import dtp1s2080jpg from "../assets/images/banner/desktop/slide-img-1-2080.jpg";
+// import dtp1s2080wpp from "../assets/images/banner/desktop/slide-img-1-2080.webp";
+// import dtp1s3120jpg from "../assets/images/banner/desktop/slide-img-1-3120.jpg";
+// import dtp1s3120wpp from "../assets/images/banner/desktop/slide-img-1-3120.webp";
+// import dtp1s4160jpg from "../assets/images/banner/desktop/slide-img-1-4160.jpg";
+// import dtp1s4160wpp from "../assets/images/banner/desktop/slide-img-1-4160.webp";
+
+const gallery = {
+  pictures: [
+    {
+      mob1xJPG: `${mob1s0418jpg}`,
+      mob1xWBP: `${mob1s0418wpp}`,
+      mob2xJPG: `${mob1s0836jpg}`,
+      mob2xWBP: `${mob1s0836wpp}`,
+      mob3xJPG: `${mob1s1254jpg}`,
+      mob3xWBP: `${mob1s1254wpp}`,
+      mob4xJPG: `${mob1s1672jpg}`,
+      mob4xWBP: `${mob1s1672wpp}`,
+      alt: {
+        en: "One of the sites of LAS, a high-quality car maintenance service",
+        uk: "Один з майданчиків LAS - якісного сервісу обслуговування автомобілів",
+      },
+    },
+    {
+      alt: {
+        en: "Maintaining the machine on a lift",
+        uk: "Обслуговування машини на підйомнику",
+      },
+    },
+    {
+      alt: {
+        en: "Comprehensive maintenance of passenger vehicles of any type",
+        uk: "Комплексне обслуговуванння легкового транспорту будь-якого виду",
+      },
+    },
+  ],
+};
+
+export default gallery;
