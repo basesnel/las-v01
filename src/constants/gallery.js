e6fd8d96d8233df88fa74626ef5f2a9f6728d84f
basesnel <basesnel@gmail.com>
2024-10-16 13:06:10 +0300

    Added images of 7 picture for mobile-media to the gallery-array.

--- src/constants/gallery.js
+++ src/constants/gallery.js
@@ -52,6 +52,15 @@ import mob06s1254wpp from "../assets/images/gallery/mobile/gallery-06-1254.webp"
 import mob06s1672jpg from "../assets/images/gallery/mobile/gallery-06-1672.jpg";
 import mob06s1672wpp from "../assets/images/gallery/mobile/gallery-06-1672.webp";
 
+import mob07s0418jpg from "../assets/images/gallery/mobile/gallery-07-418.jpg";
+import mob07s0418wpp from "../assets/images/gallery/mobile/gallery-07-418.webp";
+import mob07s0836jpg from "../assets/images/gallery/mobile/gallery-07-836.jpg";
+import mob07s0836wpp from "../assets/images/gallery/mobile/gallery-07-836.webp";
+import mob07s1254jpg from "../assets/images/gallery/mobile/gallery-07-1254.jpg";
+import mob07s1254wpp from "../assets/images/gallery/mobile/gallery-07-1254.webp";
+import mob07s1672jpg from "../assets/images/gallery/mobile/gallery-07-1672.jpg";
+import mob07s1672wpp from "../assets/images/gallery/mobile/gallery-07-1672.webp";
+
 // import tab1s0570jpg from "../assets/images/banner/tablet/slide-img-1-570.jpg";
 // import tab1s0570wpp from "../assets/images/banner/tablet/slide-img-1-570.webp";
 // import tab1s1140jpg from "../assets/images/banner/tablet/slide-img-1-1140.jpg";
@@ -159,6 +168,20 @@ const gallery = {
       mob3xWBP: `${mob06s1254wpp}`,
       mob4xJPG: `${mob06s1672jpg}`,
       mob4xWBP: `${mob06s1672wpp}`,
+      alt: {
+        en: "Comprehensive maintenance of passenger vehicles of any type",
+        uk: "Комплексне обслуговуванння легкового транспорту будь-якого виду",
+      },
+    },
+    {
+      mob1xJPG: `${mob07s0418jpg}`,
+      mob1xWBP: `${mob07s0418wpp}`,
+      mob2xJPG: `${mob07s0836jpg}`,
+      mob2xWBP: `${mob07s0836wpp}`,
+      mob3xJPG: `${mob07s1254jpg}`,
+      mob3xWBP: `${mob07s1254wpp}`,
+      mob4xJPG: `${mob07s1672jpg}`,
+      mob4xWBP: `${mob07s1672wpp}`,
       alt: {
         en: "Comprehensive maintenance of passenger vehicles of any type",
         uk: "Комплексне обслуговуванння легкового транспорту будь-якого виду",
