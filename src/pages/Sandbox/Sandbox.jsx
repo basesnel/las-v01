094e53ca74c2a601fa3705e9412846125bf5d66c
basesnel <basesnel@gmail.com>
2024-10-25 15:41:37 +0300

    Installed framer-motion.

--- src/pages/Sandbox/Sandbox.jsx
+++ src/pages/Sandbox/Sandbox.jsx
@@ -1,9 +1,21 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 const Sandbox = () => {
+  const [expanded, setExpanded] = useState(false);
   return (
     <section className={styles.sandbox}>
-      <div className={styles.box} />
+      <motion.div
+        className={styles.box}
+        onClick={() => setExpanded(!expanded)}
+        animate={{
+          width: expanded ? "calc(100vw - 4rem)" : 150,
+          opacity: 1,
+          background: "#f08",
+          borderRadius: 20,
+        }}
+      />
     </section>
   );
 };
