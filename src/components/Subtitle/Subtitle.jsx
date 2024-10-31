d29692751cb6c491828b5ad0a7b1e7203a31d0ac
basesnel <basesnel@gmail.com>
2024-10-31 19:59:38 +0200

    Added variants of motion to subtitle.

--- src/components/Subtitle/Subtitle.jsx
+++ src/components/Subtitle/Subtitle.jsx
@@ -2,15 +2,25 @@ import { motion } from "framer-motion";
 
 import styles from "./styles.module.css";
 
+const subtitleVariants = {
+  hidden: { x: 100, opacity: 0, scaleX: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scaleX: 1,
+    transition: { delay: 0.5, type: "spring", stiffness: 120 },
+  },
+};
+
 const Subtitle = ({ mode, children }) => {
   const modification = mode ? `${styles[mode]}` : "";
 
   return (
     <motion.p
       className={`${styles.subtitle} ${modification}`}
-      initial={{ x: 100, opacity: 0 }}
-      whileInView={{ x: 0, opacity: 1 }}
-      transition={{ delay: 0.5, type: "spring", stiffness: 120 }}
+      variants={subtitleVariants}
+      initial="hidden"
+      whileInView="visible"
     >
       {children}
     </motion.p>
