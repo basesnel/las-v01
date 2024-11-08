78e40b5ea595eebccb0e3e76b72628493f0be910
basesnel <basesnel@gmail.com>
2024-11-08 21:41:56 +0200

    Added parameter viewport - once: true for text.

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -2,6 +2,15 @@ import { motion } from "framer-motion";
 
 import styles from "./styles.module.css";
 
+const variants = {
+  hidden: { y: 30, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { delay: 3, duration: 0.5, type: "tween" },
+  },
+};
+
 const Text = ({ variant, mode, children }) => {
   const variation = variant ? `${styles[variant]}` : "";
   const modification = mode ? `${styles[mode]}` : "";
@@ -9,9 +18,10 @@ const Text = ({ variant, mode, children }) => {
   return (
     <motion.p
       className={`${styles.text} ${variation} ${modification}`}
-      initial={{ y: 30, opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
-      transition={{ delay: 3, duration: 0.5, type: "tween" }}
+      variants={variants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
     >
       {children}
     </motion.p>
