a582afa3ccb920c089dc4fb5409056ce06e01391
basesnel <basesnel@gmail.com>
2024-11-16 12:35:51 +0200

    Apply motion to Reference component.

--- src/components/Reference/Reference.jsx
+++ src/components/Reference/Reference.jsx
@@ -1,15 +1,69 @@
+import { motion, useAnimation } from "framer-motion";
+
 import icons from "../../assets/icons.svg";
 
 import styles from "./styles.module.css";
 
+const link = {
+  hover: {
+    scale: 1.2,
+    backgroundColor: "#f67307",
+    transition: {
+      type: "spring",
+      stiffness: 300,
+    },
+  },
+  tap: {
+    scale: 0.8,
+  },
+};
+
+const icon = {
+  initial: {
+    rotate: 0,
+    scale: 1,
+    backgroundColor: "#f67307",
+    fill: "#fff",
+  },
+  hover: {
+    rotate: 90,
+    scale: [1.2, 1],
+    backgroundColor: "#FFF",
+    fill: "#f67307",
+    transition: {
+      type: "spring",
+      stiffness: 300,
+    },
+  },
+};
+
 const Reference = (props) => {
   const { caption, src } = props;
   // const modification = mode ? `${styles[mode]}` : "";
+  const control = useAnimation();
+  const handleMouseEnter = () => {
+    control.start("hover");
+  };
+  const handleMouseLeave = () => {
+    control.start("initial");
+  };
 
   return (
-    <a href={src} className={styles.reference}>
+    <motion.a
+      variants={link}
+      initial="initial"
+      animate="animate"
+      whileHover="hover"
+      whileTap="tap"
+      href={src}
+      className={styles.reference}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       {caption}
-      <svg
+      <motion.svg
+        variants={icon}
+        animate={control}
         xmlns="http://www.w3.org/2000/svg"
         width={32}
         height={32}
@@ -17,9 +71,9 @@ const Reference = (props) => {
         aria-hidden={true}
         className={styles.icon}
       >
-        <use href={`${icons}#arrow`}></use>
-      </svg>
-    </a>
+        <use x="8" y="8" width={16} height={16} href={`${icons}#arrow`}></use>
+      </motion.svg>
+    </motion.a>
   );
 };
 
