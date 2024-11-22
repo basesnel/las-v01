b214731febc9e1e1096c556753c35a9fa55e3607
basesnel <basesnel@gmail.com>
2024-11-22 14:35:34 +0200

    Changed motion of banner-element: Data.

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -1,9 +1,31 @@
+import { motion } from "framer-motion";
+
 import styles from "./styles.module.css";
 
 const Data = ({ mode, children }) => {
   const modification = mode ? `${styles[mode]}` : "";
 
-  return <p className={`${styles.data} ${modification}`}>{children}</p>;
+  const variants = {
+    hidden: { x: -400, opacity: 0, scaleX: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      scaleX: 1,
+      transition: { delay: 0.4, type: "spring", stiffness: 300 },
+    },
+  };
+
+  return (
+    <motion.p
+      variants={variants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      className={`${styles.data} ${modification}`}
+    >
+      {children}
+    </motion.p>
+  );
 };
 
 export default Data;
