98ba912f8ab3fb5654445ad809244b056d51bebf
basesnel <basesnel@gmail.com>
2024-11-25 21:45:09 +0200

    Added useOnloadEffect for Header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import sections from "../../constants/sections";
 import Container from "../Container/Container";
 import Togglemenu from "../Togglemenu/Togglemenu";
 import Menu from "../Menu/Menu";
 import Logo from "../Logo/Logo";
 
+import sections from "../../constants/sections";
+
 import styles from "./styles.module.css";
+
+function useOnloadEffect(effectCallback) {
+  useEffect(() => {
+    window.addEventListener("load", effectCallback);
+    return () => window.removeEventListener("load", effectCallback);
+  }, []);
+}
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+
+  useOnloadEffect(() => {
+    const parts = [];
+    sections.forEach((section, i) => {
+      const part = { id: i, name: section.id, label: section.ariaLabel };
+      parts.push(part);
+    });
+    console.log("window loaded");
+    console.log(parts);
+  });
 
   const openMenu = () => {
     setShowMenu(true);
