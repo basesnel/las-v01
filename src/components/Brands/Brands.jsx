46066e7644cf0e3de61e9639996a6ba94ccd70b0
basesnel <basesnel@gmail.com>
2024-11-30 21:28:57 +0200

    Added hover and tap animates to brand-links.

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -27,6 +27,28 @@ const brandItem = {
   visible: { opacity: 1, scale: 1, x: 0, y: 0 },
 };
 
+const brandLink = {
+  initial: {
+    scale: 1,
+    rotate: 0,
+  },
+  hover: {
+    scale: [1, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2],
+    rotate: [0, 0, -13, 8, -5, 3, -2, 1, 0],
+    transition: {
+      duration: 0.5,
+      times: [0, 0.3, 0.4, 0.64, 0.8, 0.89, 0.95, 0.99, 1],
+    },
+  },
+  tap: {
+    scale: 0.8,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+    },
+  },
+};
+
 const switchStat = (i) => {
   switch (i % 4) {
     case 0:
@@ -74,14 +96,16 @@ const Brands = () => {
             }}
             viewport={{ once: true }}
           >
-            <a
+            {/* <Brand icon={icon} label={label} /> */}
+            <motion.a
               href="#"
               aria-label={label.uk}
               className={styles.link}
-              // variants={contactLink}
-              // whileHover="hover"
-              // whileFocus="hover"
-              // whileTap="tap"
+              variants={brandLink}
+              initial="initial"
+              whileHover="hover"
+              whileFocus="hover"
+              whileTap="tap"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -93,12 +117,38 @@ const Brands = () => {
               >
                 <use href={`${icons}#${icon}`}></use>
               </svg>
-            </a>
+            </motion.a>
           </motion.li>
         ))}
       </motion.ul>
     </Section>
   );
 };
+
+// const Brand = ({ icon, label }) => {
+//   return (
+//     <motion.a
+//       href="#"
+//       aria-label={label.uk}
+//       className={styles.link}
+//       variants={brandLink}
+//       initial="initial"
+//       whileHover="hover"
+//       whileFocus="hover"
+//       whileTap="tap"
+//     >
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         width={120}
+//         height={90}
+//         focusable="false"
+//         aria-hidden={true}
+//         className={styles.icon}
+//       >
+//         <use href={`${icons}#${icon}`}></use>
+//       </svg>
+//     </motion.a>
+//   );
+// };
 
 export default Brands;
