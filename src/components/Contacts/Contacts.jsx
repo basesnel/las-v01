d8640888828ee88e223a9d0d96503fe5544aa5ec
basesnel <basesnel@gmail.com>
2024-11-30 19:10:07 +0200

    Added framer motion to contacts for whileInView variant.

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,8 +1,7 @@
+import { motion } from "framer-motion";
 import Section from "../Section/Section";
 import Heading from "../Heading/Heading";
 import Subtitle from "../Subtitle/Subtitle";
-// import Link from "../Link/Link";
-// import Icon from "../Icon/Icon";
 import Text from "../Text/Text";
 import contacts from "../../constants/contacts";
 
@@ -13,6 +12,22 @@ import styles from "./styles.module.css";
 const Contacts = () => {
   const { title, list, subtitle1, text } = contacts;
 
+  const contactList = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+      },
+    },
+  };
+
+  const contactItem = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
     <Section
       colorMode="dark"
@@ -22,11 +37,26 @@ const Contacts = () => {
       <div className={styles.content}>
         <Heading level={3} title={title.uk} />
         <address className={styles.address}>
-          <ul>
+          <motion.ul
+            variants={contactList}
+            initial="hidden"
+            whileInView="visible"
+          >
             {list.map(({ id, text, src, icon }) => {
               if (id < 4)
                 return (
-                  <li key={id} className={styles.item}>
+                  <motion.li
+                    key={id}
+                    className={styles.item}
+                    variants={contactItem}
+                    initial="hidden"
+                    whileInView="visible"
+                    transition={{
+                      delay: id * 0.2,
+                      type: "spring",
+                      stiffness: 300,
+                    }}
+                  >
                     <a
                       href={src}
                       target="_blank"
@@ -45,34 +75,15 @@ const Contacts = () => {
                       </svg>
                       <span className={styles.text}>{text.uk}</span>
                     </a>
-                  </li>
+                  </motion.li>
                 );
             })}
-          </ul>
+          </motion.ul>
         </address>
       </div>
       <aside className={styles.aside}>
         <Subtitle mode="contacts">{subtitle1.uk}</Subtitle>
         <Text variant="contacts">{text.uk}</Text>
-        {/* <Subtitle mode="contacts">{subtitle2.uk}</Subtitle>
-        <ul>
-          {list.map(({ id, text, src, icon }) => {
-            if (id >= 6)
-              return (
-                <li key={id} className={styles.item}>
-                  <Link src={src} className={styles.link}>
-                    <Icon
-                      icon={icon}
-                      width={24}
-                      height={24}
-                      className={styles.icon}
-                    />
-                    <span className={styles.text}>{text.uk}</span>
-                  </Link>
-                </li>
-              );
-          })}
-        </ul> */}
       </aside>
     </Section>
   );
