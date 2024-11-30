6dc57356be8e49af0851f34a6fb55e82d0dae9f2
basesnel <basesnel@gmail.com>
2024-11-30 20:31:53 +0200

    Pre-pply markup of Brands into component for motions.

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,9 +1,12 @@
+// import { motion } from "framer-motion";
 import Section from "../Section/Section";
 import Subtitle from "../Subtitle/Subtitle";
 import Heading from "../Heading/Heading";
-import Link from "../Link/Link";
-import Icon from "../Icon/Icon";
+// import Link from "../Link/Link";
+// import Icon from "../Icon/Icon";
 import brands from "../../constants/brands";
+
+import icons from "../../assets/icons.svg";
 
 import styles from "./styles.module.css";
 
@@ -17,14 +20,34 @@ const Brands = () => {
       <ul className={styles.list}>
         {carBrands.map(({ icon, label }, i) => (
           <li key={i} className={styles.item}>
-            <Link src="#" label={label.uk} className={styles.link}>
+            {/* <Link src="#" label={label.uk} className={styles.link}>
               <Icon
                 icon={icon}
                 width={120}
                 height={90}
                 className={styles.icon}
               />
-            </Link>
+            </Link> */}
+            <a
+              href="#"
+              aria-label={label.uk}
+              className={styles.link}
+              // variants={contactLink}
+              // whileHover="hover"
+              // whileFocus="hover"
+              // whileTap="tap"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={120}
+                height={90}
+                focusable="false"
+                aria-hidden={true}
+                className={styles.icon}
+              >
+                <use href={`${icons}#${icon}`}></use>
+              </svg>
+            </a>
           </li>
         ))}
       </ul>
