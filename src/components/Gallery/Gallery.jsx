a43b47efdad52fb4730638569f10de85e4d4d8f0
basesnel <basesnel@gmail.com>
2024-12-09 20:09:37 +0200

    Applied initial variant of animation for Gallery.

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,11 @@
-import { motion } from "framer-motion";
+import { useState } from "react";
+import useMeasure from "react-use-measure";
+import {
+  animate,
+  AnimatePresence,
+  motion,
+  useMotionValue,
+} from "framer-motion";
 import Section from "../Section/Section";
 import Heading from "../Heading/Heading";
 import Subtitle from "../Subtitle/Subtitle";
@@ -6,10 +13,27 @@ import gallery from "../../constants/gallery";
 
 import styles from "./styles.module.css";
 import Container from "../Container/Container";
-import { useState } from "react";
+import { useEffect } from "react";
 
 const Gallery = () => {
   const { subTitle, title, images } = gallery;
+
+  let [ref, { width }] = useMeasure();
+
+  const xTransition = useMotionValue(0);
+
+  useEffect(() => {
+    let controls;
+    let finalPosition = -width / 2 - 12;
+    controls = animate(xTransition, [0, finalPosition], {
+      ease: "linear",
+      duration: 25,
+      repeat: Infinity,
+      repeatDelay: 0,
+    });
+
+    return controls.stop;
+  }, [xTransition, width]);
 
   return (
     <Section indexSection={6}>
@@ -17,11 +41,15 @@ const Gallery = () => {
         <Subtitle mode="gallery">{subTitle.uk}</Subtitle>
         <Heading level={2} mode="hidden" title={title.uk} />
       </Container>
-      <div className={styles.galleryContainer}>
-        {images.map((image, i) => (
+      <motion.div
+        className={styles.galleryContainer}
+        ref={ref}
+        style={{ x: xTransition }}
+      >
+        {[...images, ...images].map((image, i) => (
           <Card key={i} image={image} />
         ))}
-      </div>
+      </motion.div>
       {/* <ul className={styles.list}>
         {images.map((image, i) => (
           <li key={i} className={styles.item}>
@@ -93,12 +121,26 @@ const Card = ({ image }) => {
       onHoverStart={() => setShowOverlay(true)}
       onHoverEnd={() => setShowOverlay(false)}
     >
-      {showOverlay && (
-        <div className={styles.cardOverlay}>
-          <div className={styles.bgBlack} />
-          <span className={styles.label}>{image.alt.uk}</span>
-        </div>
-      )}
+      <AnimatePresence>
+        {showOverlay && (
+          <motion.div
+            className={styles.cardOverlay}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 0.5 } }}
+            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+          >
+            <div className={styles.bgBlack} />
+            <span
+              className={styles.label}
+              initial={{ y: 20 }}
+              animate={{ y: 0, transition: { duration: 0.5 } }}
+              exit={{ y: 20, transition: { duration: 0.5 } }}
+            >
+              {image.alt.uk}
+            </span>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <picture>
         <source
           srcSet={`${image.dtp1xWBP} 1200w, ${image.dtp2xWBP} 2400w, ${image.dtp3xWBP} 3600w, ${image.dtp2xWBP} 4800w`}
