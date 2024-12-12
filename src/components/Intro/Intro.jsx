55703e2526654a9934fdd96f218d8280caafa89c
basesnel <basesnel@gmail.com>
2024-12-12 14:56:24 +0200

    Apply animation for Intro-images list.

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,14 +1,20 @@
+import { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 import Section from "../Section/Section";
 import Subtitle from "../Subtitle/Subtitle";
 import Heading from "../Heading/Heading";
 import Text from "../Text/Text";
+
 import Reference from "../Reference/Reference";
 import intro from "../../constants/intro";
 
 import styles from "./styles.module.css";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useRef } from "react";
+
+const spring = {
+  type: "spring",
+  dumping: 20,
+  stiffness: 100,
+};
 
 const Intro = () => {
   const {
@@ -24,20 +30,6 @@ const Intro = () => {
 
   const [images, setImages] = useState(abouts);
 
-  // console.log(images);
-
-  // var shuffle = (a) =>
-  //   a.length ? a.splice(~~(Math.random() * a.length), 1).concat(shuffle(a)) : a;
-
-  // const shuffle = (a) => {
-  //   return a
-  //     .map((value) => ({ value, sort: Math.random() }))
-  //     .sort((a, b) => a.sort - b.sort)
-  //     .map(({ value }) => value);
-  // };
-
-  // console.log("Shuffle out of Effect:", shuffle(images));
-
   const intervalId = useRef(null);
 
   useEffect(() => {
@@ -50,12 +42,11 @@ const Intro = () => {
 
     intervalId.current = setInterval(() => {
       setImages((prevImages) => [...shuffle(prevImages)]);
-      console.log("Effect images:", images);
+      console.log("Effecting images...");
     }, 10000);
 
     return () => {
       console.log("This is clear function to the effect trigger");
-      console.log("Images on clear effect:", images);
       clearInterval(intervalId.current);
     };
   }, []);
@@ -75,7 +66,7 @@ const Intro = () => {
         <ul className={styles.list}>
           {images.map((about) => {
             return (
-              <li key={about.id} className={styles.item}>
+              <motion.li key={about.id} className={styles.item} layout={spring}>
                 <div className={styles.thumb}>
                   <picture>
                     <source
@@ -112,51 +103,9 @@ const Intro = () => {
                     />
                   </picture>
                 </div>
-              </li>
+              </motion.li>
             );
           })}
-          {/* {abouts.map((about) => {
-            return (
-              <li key={about.id} className={styles.item}>
-                <div className={styles.thumb}>
-                  <picture>
-                    <source
-                      srcSet={`${about.dtp1xWBP} 1200w, ${about.dtp2xWBP} 2400w, ${about.dtp3xWBP} 3600w, ${about.dtp2xWBP} 4800w`}
-                      media="(min-width: 1200px)"
-                      width={270}
-                      height={445}
-                      type="image/webp"
-                    />
-
-                    <source
-                      srcSet={`${about.dtp1xJPG} 1200w, ${about.dtp2xJPG} 2400w, ${about.dtp3xJPG} 3600w, ${about.dtp2xJPG} 4800w`}
-                      media="(min-width: 1200px)"
-                      width={270}
-                      height={445}
-                      type="image/jpeg"
-                    />
-
-                    <source
-                      srcSet={`${about.tab1xWBP} 768w, ${about.tab2xWBP} 1536w, ${about.tab3xWBP} 2304w, ${about.tab2xWBP} 3072w`}
-                      width={330}
-                      height={235}
-                      type="image/webp"
-                    />
-
-                    <img
-                      loading="lazy"
-                      srcSet={`${about.tab1xJPG} 768w, ${about.tab2xJPG} 1536w, ${about.tab3xJPG} 2304w, ${about.tab2xJPG} 3072w`}
-                      src={about.tab3xJPG}
-                      alt={about.alt.uk}
-                      width={330}
-                      height={235}
-                      className={styles.image}
-                    />
-                  </picture>
-                </div>
-              </li>
-            );
-          })} */}
         </ul>
       </aside>
     </Section>
