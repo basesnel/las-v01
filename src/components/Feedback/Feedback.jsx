8125ca753038a90ac76b19fe09b854f544fec9e0
basesnel <basesnel@gmail.com>
2024-12-16 13:26:40 +0200

    Created Form-component separately.

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -7,6 +7,20 @@ import styles from "./styles.module.css";
 
 const Feedback = () => {
   const { title, form } = feedback;
+
+  return (
+    <Section
+      colorMode="dark"
+      sectionVariant="feedback"
+      containerVariant="feedback"
+    >
+      <Heading level={3} title={title.uk} />
+      <Form form={form} />
+    </Section>
+  );
+};
+
+const Form = ({ form }) => {
   const { name, phone, message, send, reset } = form;
 
   const onSubmitFeedback = (e) => {
@@ -18,49 +32,38 @@ const Feedback = () => {
   };
 
   return (
-    <Section
-      colorMode="dark"
-      sectionVariant="feedback"
-      containerVariant="feedback"
+    <form
+      className={styles.form}
+      autoComplete="off"
+      onSubmit={onSubmitFeedback}
     >
-      <Heading level={3} title={title.uk} />
-      <form
-        className={styles.form}
-        autoComplete="off"
-        onSubmit={onSubmitFeedback}
-      >
-        <label className={styles.field}>
-          <input
-            type="text"
-            name="name"
-            className={styles.input}
-            placeholder=" "
-            autoComplete="off"
-          />
-          <span className={`${styles.label} ${styles.req}`}>{name.uk}</span>
-        </label>
-        <label className={styles.field}>
-          <input
-            type="tel"
-            name="tel"
-            className={styles.input}
-            placeholder=" "
-            autoComplete="off"
-          />
-          <span className={`${styles.label} ${styles.req}`}>{phone.uk}</span>
-        </label>
-        <label className={styles.field}>
-          <textarea
-            name="message"
-            className={styles.textarea}
-            placeholder=" "
-          />
-          <span className={`${styles.label}`}>{message.uk}</span>
-        </label>
-        <Button type="submit" caption={send.uk} />
-        <Button type="reset" caption={reset.uk} />
-      </form>
-    </Section>
+      <label className={styles.field}>
+        <input
+          type="text"
+          name="name"
+          className={styles.input}
+          placeholder=" "
+          autoComplete="off"
+        />
+        <span className={`${styles.label} ${styles.req}`}>{name.uk}</span>
+      </label>
+      <label className={styles.field}>
+        <input
+          type="tel"
+          name="tel"
+          className={styles.input}
+          placeholder=" "
+          autoComplete="off"
+        />
+        <span className={`${styles.label} ${styles.req}`}>{phone.uk}</span>
+      </label>
+      <label className={styles.field}>
+        <textarea name="message" className={styles.textarea} placeholder=" " />
+        <span className={`${styles.label}`}>{message.uk}</span>
+      </label>
+      <Button type="submit" caption={send.uk} />
+      <Button type="reset" caption={reset.uk} />
+    </form>
   );
 };
 
