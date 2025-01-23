77e9033ea02502f219fb2a98ba0f8606d022f21d
basesnel <basesnel@gmail.com>
2025-01-23 20:27:50 +0200

    Added throttle of lodash to project.

--- src/components/Sandbox/SimpleSlider/SimpleSlider.jsx
+++ src/components/Sandbox/SimpleSlider/SimpleSlider.jsx
@@ -1,20 +1,37 @@
 import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
+import { throttle } from "lodash";
 import gallery from "../../../constants/gallery";
 
 import styles from "./styles.module.css";
 
 const SimpleSlider = () => {
-  const constraintRef = useRef();
+  const carousel = useRef();
 
   const { images } = gallery;
 
+  const getPositionOfContainer = () => {
+    const { x, left, right } =
+      carousel.current.firstChild.getBoundingClientRect();
+    console.log(x, left, right);
+  };
+
+  useEffect(() => {
+    console.log(carousel.current.offsetWidth, carousel.current.scrollWidth);
+    console.log(carousel.current.firstChild.getBoundingClientRect());
+
+    // for (var key in carousel.current) {
+    //   console.log(key, carousel.current[key]);
+    // }
+  }, []);
+
   return (
-    <motion.div className={styles.carousel} ref={constraintRef}>
+    <motion.div className={styles.carousel} ref={carousel}>
       <motion.div
         className={styles.imagesContainer}
         drag="x"
-        dragConstraints={constraintRef}
+        onDrag={throttle(getPositionOfContainer, 1000)}
+        // dragConstraints={constraintRef}
       >
         {[...images, ...images].map((image, index) => {
           return (
