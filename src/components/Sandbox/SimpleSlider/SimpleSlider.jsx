7ffd120d43f7ddb2495fa7c1a4fb982bc6cf9503
basesnel <basesnel@gmail.com>
2025-01-23 16:18:19 +0200

    Organazed code of SimpleSlider component.

--- src/components/Sandbox/SimpleSlider/SimpleSlider.jsx
+++ src/components/Sandbox/SimpleSlider/SimpleSlider.jsx
@@ -5,33 +5,18 @@ import gallery from "../../../constants/gallery";
 import styles from "./styles.module.css";
 
 const SimpleSlider = () => {
-  // const [width, setWidth] = useState(0);
   const constraintRef = useRef();
 
   const { images } = gallery;
 
-  // useEffect(() => {
-  //   console.log(
-  //     constraintRef.current.scrollWidth,
-  //     constraintRef.current.offsetWidth
-  //   );
-  //   setWidth(
-  //     constraintRef.current.scrollWidth - constraintRef.current.offsetWidth
-  //   );
-  // }, []);
-
   return (
-    <motion.div
-      className={styles.carousel}
-      ref={constraintRef}
-      whileTap={{ cursor: "grabbing" }}
-    >
+    <motion.div className={styles.carousel} ref={constraintRef}>
       <motion.div
-        className={styles.imagesSliderContainer}
+        className={styles.imagesContainer}
         drag="x"
         dragConstraints={constraintRef}
       >
-        {images.map((image, index) => {
+        {[...images, ...images].map((image, index) => {
           return (
             <div key={index} className={styles.item}>
               <img
