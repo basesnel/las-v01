909d093e25061d06b7756dec403f9bbbc5b41a54
basesnel <basesnel@gmail.com>
2025-01-27 21:19:02 +0200

    Some order in the Sandbox.

--- src/pages/Sandbox/Sandbox.jsx
+++ src/pages/Sandbox/Sandbox.jsx
@@ -16,12 +16,15 @@ const Sandbox = () => {
         <h2 className={styles.header}>easy image gallery slider</h2>
         <EasySlider />
       </section>
-      <section className={styles.sandbox}>
+      <section className={styles.section}>
+        <h2 className={styles.header}>simple slider</h2>
         <Carousel>
           <div className={`${styles.item} ${styles.item1}`}>Item 1</div>
           <div className={`${styles.item} ${styles.item2}`}>Item 2</div>
           <div className={`${styles.item} ${styles.item3}`}>Item 3</div>
         </Carousel>
+      </section>
+      <section className={styles.sandbox}>
         <CarouselCompound infinite>
           <CarouselCompound.Page>
             <div className={`${styles.item} ${styles.item1}`}>Item 1</div>
