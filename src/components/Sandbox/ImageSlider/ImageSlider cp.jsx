985911783a24c8102467b092d18548766b62a7fb
basesnel <basesnel@gmail.com>
2025-01-28 15:36:41 +0200

    Added all 12 images to ImageSlider in the Sandbox.

--- src/components/Sandbox/ImageSlider/ImageSlider cp.jsx
+++ src/components/Sandbox/ImageSlider/ImageSlider cp.jsx
@@ -5,16 +5,12 @@ import gallery from "../../../constants/gallery";
 import styles from "./styles.module.css";
 
 const ImageSlider = () => {
-  const [positionIndexes, setPositionIndexes] = useState([
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-  ]);
+  const [positionIndexes, setPositionIndexes] = useState([0, 1, 2, 3, 4]);
   const { images } = gallery;
 
   const handleNext = () => {
     setPositionIndexes((prevIndexes) => {
-      const updateIndexes = prevIndexes.map(
-        (prevIndex) => (prevIndex + 1) % 12
-      );
+      const updateIndexes = prevIndexes.map((prevIndex) => (prevIndex + 1) % 5);
       return updateIndexes;
     });
   };
@@ -22,57 +18,37 @@ const ImageSlider = () => {
   const handlePrev = () => {
     setPositionIndexes((prevIndexes) => {
       const updateIndexes = prevIndexes.map((prevIndex) =>
-        prevIndex === 0 ? (prevIndex + 11) % 12 : (prevIndex - 1) % 12
+        prevIndex === 0 ? (prevIndex + 4) % 5 : (prevIndex - 1) % 5
       );
       return updateIndexes;
     });
   };
 
-  // const galleryImages = [
-  //   images[0].tab2xJPG,
-  //   images[1].tab2xJPG,
-  //   images[2].tab2xJPG,
-  //   images[3].tab2xJPG,
-  //   images[4].tab2xJPG,
-  // ];
-
-  const positions = [
-    "center",
-    "left4",
-    "left3",
-    "left2",
-    "left1",
-    "left",
-    "behind",
-    "right",
-    "right1",
-    "right2",
-    "right3",
-    "right4",
+  const galleryImages = [
+    images[0].tab2xJPG,
+    images[1].tab2xJPG,
+    images[2].tab2xJPG,
+    images[3].tab2xJPG,
+    images[4].tab2xJPG,
   ];
 
+  const positions = ["center", "left1", "left", "right", "right1"];
+
   const imageVariants = {
-    center: { x: "0%", scale: 1, zIndex: 8 },
-    left4: { x: "-50%", scale: 0.8, zIndex: 5 },
-    left3: { x: "-75%", scale: 0.5, zIndex: 3 },
-    left2: { x: "-90%", scale: 0.3, zIndex: 1 },
-    left1: { x: "-100%", scale: 0, zIndex: 0 },
-    left: { x: "-100%", scale: 0, zIndex: 0 },
-    behind: { x: "0%", scale: 0, zIndex: 0 },
-    right: { x: "100%", scale: 0, zIndex: 0 },
-    right1: { x: "100%", scale: 0, zIndex: 0 },
-    right2: { x: "90%", scale: 0.3, zIndex: 1 },
-    right3: { x: "75%", scale: 0.5, zIndex: 3 },
-    right4: { x: "50%", scale: 0.8, zIndex: 5 },
+    center: { x: "0%", scale: 1, zIndex: 5 },
+    left1: { x: "-50%", scale: 0.7, zIndex: 2 },
+    left: { x: "-90%", scale: 0.5, zIndex: 1 },
+    right: { x: "90%", scale: 0.5, zIndex: 1 },
+    right1: { x: "50%", scale: 0.7, zIndex: 2 },
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.imageSlider}>
-        {images.map((image, index) => (
+        {galleryImages.map((image, index) => (
           <motion.img
             key={index}
-            src={image.tab2xJPG}
+            src={image}
             alt={index}
             className={styles.image}
             initial="center"
