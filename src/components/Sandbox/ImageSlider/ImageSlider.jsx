149c4059ec85d91300b6f1978775109fd74aee35
basesnel <basesnel@gmail.com>
2025-01-30 16:54:26 +0200

    Added figure and picture instead img in the ImageSlider.

--- src/components/Sandbox/ImageSlider/ImageSlider.jsx
+++ src/components/Sandbox/ImageSlider/ImageSlider.jsx
@@ -73,17 +73,83 @@ const ImageSlider = () => {
     <div className={styles.container}>
       <div className={styles.imageSlider}>
         {images.map((image, index) => (
-          <motion.img
+          // <motion.img
+          //   key={index}
+          //   src={image.tab2xJPG}
+          //   alt={index}
+          //   className={styles.image}
+          //   initial="center"
+          //   animate={positions[positionIndexes[index]]}
+          //   variants={imageVariants}
+          //   transition={{ duration: 0.5 }}
+          //   style={{ position: "absolute" }}
+          // />
+          <motion.figure
             key={index}
-            src={image.tab2xJPG}
-            alt={index}
-            className={styles.image}
+            className={styles.card}
             initial="center"
             animate={positions[positionIndexes[index]]}
             variants={imageVariants}
             transition={{ duration: 0.5 }}
             style={{ position: "absolute" }}
-          />
+          >
+            <picture>
+              <source
+                srcSet={`${image.dtp1xWBP} 418w, ${image.dtp2xWBP} 836w, ${image.dtp3xWBP} 1254w, ${image.dtp4xWBP} 1672w`}
+                sizes="418px"
+                media="(min-width: 1200px)"
+                width={418}
+                height={360}
+                type="image/webp"
+              />
+
+              <source
+                srcSet={`${image.dtp1xJPG} 418w, ${image.dtp2xJPG} 836w, ${image.dtp3xJPG} 1254w, ${image.dtp4xJPG} 1672w`}
+                sizes="418px"
+                media="(min-width: 1200px)"
+                width={418}
+                height={360}
+                type="image/jpeg"
+              />
+
+              <source
+                srcSet={`${image.tab1xWBP} 330w, ${image.tab2xWBP} 660w, ${image.tab3xWBP} 990w, ${image.tab4xWBP} 1320w`}
+                sizes="330px"
+                media="(min-width: 768px) and (max-width: 1199px)"
+                width={330}
+                height={360}
+                type="image/webp"
+              />
+
+              <source
+                srcSet={`${image.tab1xJPG} 330w, ${image.tab2xJPG} 660w, ${image.tab3xJPG} 990w, ${image.tab4xJPG} 1320w`}
+                sizes="330px"
+                media="(min-width: 768px) and (max-width: 1199px)"
+                width={330}
+                height={360}
+                type="image/jpeg"
+              />
+
+              <source
+                srcSet={`${image.mob1xWBP} 270w, ${image.mob2xWBP} 540w, ${image.mob3xWBP} 810w, ${image.mob4xWBP} 1080w`}
+                sizes="(max-width: 480px) calc(100vw - 110px), 270px"
+                width={270}
+                height={360}
+                type="image/webp"
+              />
+
+              <img
+                loading="lazy"
+                srcSet={`${image.mob1xJPG} 270w, ${image.mob2xJPG} 540w, ${image.mob3xJPG} 810w, ${image.mob4xJPG} 1080w`}
+                sizes="(max-width: 480px) calc(100vw - 110px), 270px"
+                src={image.mob2xJPG}
+                alt={image.alt.uk}
+                width={270}
+                height={360}
+                className={styles.image}
+              />
+            </picture>
+          </motion.figure>
         ))}
       </div>
       <div className={styles.controls}>
