c230cbb0c40e0a896f4346c241dc273c7d4c7230
basesnel <basesnel@gmail.com>
2025-01-31 19:36:13 +0200

    Corrected laptop variants.

--- src/components/Sandbox/ImageSlider/ImageSlider.jsx
+++ src/components/Sandbox/ImageSlider/ImageSlider.jsx
@@ -18,7 +18,10 @@ const ImageSlider = () => {
     tabletFirst: "(min-width: 481px) and (max-width: 590px)",
     tabletSecond: "(min-width: 591px) and (max-width: 700px)",
     tabletThird: "(min-width: 701px) and (max-width: 768px)",
-    laptop: "(min-width: 769px) and (max-width: 1199px)",
+    laptopFirst: "(min-width: 769px) and (max-width: 860px)",
+    laptopSecond: "(min-width: 861px) and (max-width: 960px)",
+    laptopThird: "(min-width: 961px) and (max-width: 1100px)",
+    laptopFourth: "(min-width: 1101px) and (max-width: 1199px)",
     desktop: "(min-width: 1200px)",
   };
 
@@ -140,29 +143,74 @@ const ImageSlider = () => {
       left4: { x: "-60%", scale: 0.8, opacity: 0.8, zIndex: 5 },
       left3: { x: "-100%", scale: 0.5, opacity: 0.5, zIndex: 3 },
       left2: { x: "-135%", scale: 0, opacity: 0, zIndex: 1 },
-      left1: { x: "-100%", scale: 0, opacity: 0, zIndex: 0 },
-      left: { x: "-100%", scale: 0, opacity: 0, zIndex: 0 },
+      left1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      left: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
       behind: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
-      right: { x: "100%", scale: 0, opacity: 0, zIndex: 0 },
-      right1: { x: "100%", scale: 0, opacity: 0, zIndex: 0 },
+      right: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
       right2: { x: "135%", scale: 0, opacity: 0, zIndex: 1 },
       right3: { x: "100%", scale: 0.5, opacity: 0.5, zIndex: 3 },
       right4: { x: "60%", scale: 0.8, opacity: 0.8, zIndex: 5 },
     };
 
-    const variantsLaptop = {
+    const variantsLaptopFirst = {
+      center: { x: "0%", scale: 1, opacity: 1, zIndex: 8 },
+      left4: { x: "-50%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+      left3: { x: "-90%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      left2: { x: "-135%", scale: 0, opacity: 0, zIndex: 1 },
+      left1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      left: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      behind: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right2: { x: "135%", scale: 0, opacity: 0, zIndex: 1 },
+      right3: { x: "90%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      right4: { x: "50%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+    };
+
+    const variantsLaptopSecond = {
       center: { x: "0%", scale: 1, opacity: 1, zIndex: 8 },
       left4: { x: "-60%", scale: 0.8, opacity: 0.8, zIndex: 5 },
       left3: { x: "-100%", scale: 0.5, opacity: 0.5, zIndex: 3 },
-      left2: { x: "-135%", scale: 0, opacity: 0, zIndex: 1 },
+      left2: { x: "-140%", scale: 0, opacity: 0, zIndex: 1 },
+      left1: { x: "-0%", scale: 0, opacity: 0, zIndex: 0 },
+      left: { x: "-0%", scale: 0, opacity: 0, zIndex: 0 },
+      behind: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right2: { x: "140%", scale: 0, opacity: 0, zIndex: 1 },
+      right3: { x: "100%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      right4: { x: "60%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+    };
+
+    const variantsLaptopThird = {
+      center: { x: "0%", scale: 1, opacity: 1, zIndex: 8 },
+      left4: { x: "-70%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+      left3: { x: "-115%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      left2: { x: "-150%", scale: 0, opacity: 0, zIndex: 1 },
+      left1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      left: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      behind: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right1: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
+      right2: { x: "150%", scale: 0, opacity: 0, zIndex: 1 },
+      right3: { x: "115%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      right4: { x: "70%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+    };
+
+    const variantsLaptopFourth = {
+      center: { x: "0%", scale: 1, opacity: 1, zIndex: 8 },
+      left4: { x: "-80%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+      left3: { x: "-135%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      left2: { x: "-180%", scale: 0, opacity: 0, zIndex: 1 },
       left1: { x: "-100%", scale: 0, opacity: 0, zIndex: 0 },
       left: { x: "-100%", scale: 0, opacity: 0, zIndex: 0 },
       behind: { x: "0%", scale: 0, opacity: 0, zIndex: 0 },
       right: { x: "100%", scale: 0, opacity: 0, zIndex: 0 },
       right1: { x: "100%", scale: 0, opacity: 0, zIndex: 0 },
-      right2: { x: "135%", scale: 0, opacity: 0, zIndex: 1 },
-      right3: { x: "100%", scale: 0.5, opacity: 0.5, zIndex: 3 },
-      right4: { x: "60%", scale: 0.8, opacity: 0.8, zIndex: 5 },
+      right2: { x: "180%", scale: 0, opacity: 0, zIndex: 1 },
+      right3: { x: "135%", scale: 0.5, opacity: 0.5, zIndex: 3 },
+      right4: { x: "80%", scale: 0.8, opacity: 0.8, zIndex: 5 },
     };
 
     const imageVariantsDesktop = {
@@ -190,7 +238,13 @@ const ImageSlider = () => {
 
     if (media.tabletThird) return variantsTabletThirdStep;
 
-    if (media.laptop) return variantsLaptop;
+    if (media.laptopFirst) return variantsLaptopFirst;
+
+    if (media.laptopSecond) return variantsLaptopSecond;
+
+    if (media.laptopThird) return variantsLaptopThird;
+
+    if (media.laptopFourth) return variantsLaptopFourth;
 
     if (media.desktop) return imageVariantsDesktop;
   };
