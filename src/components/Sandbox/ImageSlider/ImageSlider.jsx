91fe269d3327c0d11205ef2d1d97ba091b29f6d8
basesnel <basesnel@gmail.com>
2025-02-03 16:09:23 +0200

    Another experiment with swipeConfidenceThreshold and offset to reduce spring effect.

--- src/components/Sandbox/ImageSlider/ImageSlider.jsx
+++ src/components/Sandbox/ImageSlider/ImageSlider.jsx
@@ -7,7 +7,7 @@ import getVariants from "./getVariants";
 
 import styles from "./styles.module.css";
 
-const swipeConfidenceThreshold = 70000;
+const swipeConfidenceThreshold = 65000;
 const swipePower = (offset, velocity) => {
   return Math.abs(offset) * velocity;
 };
