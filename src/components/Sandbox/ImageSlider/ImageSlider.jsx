be9a4cb572d3e5bd595d5f5ddcb2c6195b672bda
basesnel <basesnel@gmail.com>
2025-02-04 11:37:49 +0200

    Reduce throttle interval to 280ms for gallery dragging.

--- src/components/Sandbox/ImageSlider/ImageSlider.jsx
+++ src/components/Sandbox/ImageSlider/ImageSlider.jsx
@@ -124,7 +124,7 @@ const ImageSlider = () => {
               swipeConfidenceThreshold,
             });
           }
-        }, 360)}
+        }, 280)}
       >
         {images.map((image, index) => (
           <motion.figure
