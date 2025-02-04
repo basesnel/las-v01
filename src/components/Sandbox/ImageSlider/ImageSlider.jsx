c758f14f5a44d86eceb3e321ef8e6d2007a0532b
basesnel <basesnel@gmail.com>
2025-02-04 11:34:24 +0200

    Increase throttle interval to 360ms for gallery dragging.

--- src/components/Sandbox/ImageSlider/ImageSlider.jsx
+++ src/components/Sandbox/ImageSlider/ImageSlider.jsx
@@ -124,7 +124,7 @@ const ImageSlider = () => {
               swipeConfidenceThreshold,
             });
           }
-        }, 200)}
+        }, 360)}
       >
         {images.map((image, index) => (
           <motion.figure
