21564ba1f9bdf5b7e08defb41d262656001f9121
basesnel <basesnel@gmail.com>
2025-02-19 09:19:02 +0200

    Fixed screen-image position on tablet-media.

--- src/components/TowTruck/TowTruck.jsx
+++ src/components/TowTruck/TowTruck.jsx
@@ -12,12 +12,6 @@ import towtruck from "../../constants/towtruck";
 
 import styles from "./styles.module.css";
 
-const spring = {
-  type: "spring",
-  dumping: 20,
-  stiffness: 100,
-};
-
 const TowTruck = () => {
   const {
     subTitle,
@@ -53,46 +47,5 @@ const TowTruck = () => {
     </Section>
   );
 };
-
-// const List = ({ list }) => {
-//   const ref = useRef(null);
-//   const isInView = useInView(ref, { amount: 0.8, once: true });
-
-//   const transportItem = {
-//     hidden: { scale: 0, opacity: 0 },
-//     visible: {
-//       scale: 1,
-//       opacity: 1,
-//     },
-//   };
-
-//   return (
-//     <ul ref={ref} className={styles.list}>
-//       {list.map(({ uk }, i) => {
-//         return (
-//           <motion.li
-//             key={i}
-//             className={styles.item}
-//             variants={transportItem}
-//             style={
-//               i % 2
-//                 ? { originX: 1, originY: 0.5 }
-//                 : { originX: 0, originY: 0.5 }
-//             }
-//             initial="hidden"
-//             animate={isInView && "visible"}
-//             transition={{
-//               type: "tween",
-//               duration: 0.5,
-//               delay: 0.4,
-//             }}
-//           >
-//             {uk}
-//           </motion.li>
-//         );
-//       })}
-//     </ul>
-//   );
-// };
 
 export default TowTruck;
