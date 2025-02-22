fa02aaa738dfb7b29cbdd64edd642a15e8f0ceea
basesnel <basesnel@gmail.com>
2025-02-22 16:53:47 +0200

    Apply some skeletons.

---
+++ src/pages/Main/Main copy 2.jsx
@@ -0,0 +1,35 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
+import Intro from "../../components/Intro/Intro";
+import Services from "../../components/Services/Services";
+import Features from "../../components/Features/Features";
+import Gallery from "../../components/Gallery/Gallery";
+import ContactUs from "../../components/ContactUs/ContactUs";
+import Brands from "../../components/Brands/Brands";
+import TowTruck from "../../components/TowTruck/TowTruck";
+// import Shop from "../../components/Shop/Shop";
+
+// import styles from "./styles.module.css";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Banner />
+        <Intro />
+        <Services />
+        <TowTruck />
+        <Gallery />
+        <Features />
+        {/* <Shop /> */}
+        <ContactUs />
+        <Brands />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
@@ -7,22 +9,81 @@ import Features from "../../components/Features/Features";
 import Gallery from "../../components/Gallery/Gallery";
 import ContactUs from "../../components/ContactUs/ContactUs";
 import Brands from "../../components/Brands/Brands";
+import HeaderSkeleton from "../../components/HeaderSkeleton/HeaderSkeleton";
+import BannerSkeleton from "../../components/BannerSkeleton/BannerSkeleton";
+import IntroSkeleton from "../../components/IntroSkeleton/IntroSkeleton";
+import ServicesSkeleton from "../../components/ServicesSkeleton/ServicesSkeleton";
+import GalletySkeleton from "../../components/GallerySkeleton/GallerySkeleton";
+import FeaturesSkeleton from "../../components/FeaturesSkeleton/FeaturesSkeleton";
 import TowTruck from "../../components/TowTruck/TowTruck";
 // import Shop from "../../components/Shop/Shop";
 
 // import styles from "./styles.module.css";
 
 const Main = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <Header />
+      <AnimatePresence mode="wait">
+        {isLoading ? (
+          <motion.div
+            key="loadingHeader"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <HeaderSkeleton />
+          </motion.div>
+        ) : (
+          <Header />
+        )}
+      </AnimatePresence>
+
+      {/* {isLoading ? null : <Header />} */}
+      {/* <Header /> */}
+
       <main>
-        <Banner />
+        <AnimatePresence mode="wait">
+          {isLoading ? (
+            <motion.div
+              key="bannerLoading"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <BannerSkeleton />
+              <IntroSkeleton />
+              <ServicesSkeleton />
+              <GalletySkeleton />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="bannerLoaded"
+              initial={{ opacity: 0, scale: 0.3 }}
+              animate={{ opacity: 1, scale: 1 }}
+            >
+              <Banner />
+              <Intro />
+              <Services />
+              <TowTruck />
+              <Gallery />
+            </motion.div>
+          )}
+        </AnimatePresence>
+        {/* <Banner />
         <Intro />
         <Services />
         <TowTruck />
-        <Gallery />
+        <Gallery /> */}
         <Features />
+        {/* <FeaturesSkeleton /> */}
         {/* <Shop /> */}
         <ContactUs />
         <Brands />
