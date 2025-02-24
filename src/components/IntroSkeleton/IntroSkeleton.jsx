6f93fa92422bc6b84adce79955d7f40266ef0fd3
basesnel <basesnel@gmail.com>
2025-02-24 08:18:21 +0200

    Fixed styles of IntroSkeleton for medias: mobile-tablet.

--- src/components/IntroSkeleton/IntroSkeleton.jsx
+++ src/components/IntroSkeleton/IntroSkeleton.jsx
@@ -1,16 +1,16 @@
-import { motion, easeOut, useTime, useTransform } from "framer-motion";
+// import { motion, easeOut, useTime, useTransform } from "framer-motion";
 
 import styles from "./styles.module.css";
 
 const IntroSkeleton = () => {
-  const time = useTime();
-  const pulseOpacity = useTransform(time, [0, 1000, 2000, 3000], [1, 0, 1, 0], {
-    ease: easeOut,
-  });
+  // const time = useTime();
+  // const pulseOpacity = useTransform(time, [0, 1000, 2000, 3000], [1, 0, 1, 0], {
+  //   ease: easeOut,
+  // });
 
   return (
-    <motion.div className={styles.section} style={{ opacity: pulseOpacity }}>
-      {/* <div className={styles.section}> */}
+    // <motion.div className={styles.section} style={{ opacity: pulseOpacity }}>
+    <div className={styles.section}>
       <div className={styles.container}>
         <div className={styles.content}>
           <div className={styles.subtitle} />
@@ -22,6 +22,7 @@ const IntroSkeleton = () => {
             <div className={styles.text} />
           </div>
           <div className={styles.paragraph}>
+            <div className={styles.text} />
             <div className={styles.text} />
             <div className={styles.text} />
             <div className={styles.text} />
@@ -38,8 +39,8 @@ const IntroSkeleton = () => {
           <div className={styles.img} />
         </div>
       </div>
-      {/* </div> */}
-    </motion.div>
+    </div>
+    // </motion.div>
   );
 };
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -54,7 +54,9 @@ const Main = () => {
           <Header />
           <main>
             <Banner />
+            <BannerSkeleton />
             <Intro />
+            <IntroSkeleton />
             <Services />
             <TowTruck />
             <Gallery />
