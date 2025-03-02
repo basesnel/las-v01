edc4e8eaba9cc4ddb939c48ba485f36f345ee4b6
basesnel <basesnel@gmail.com>
2025-03-02 12:34:52 +0200

    Styled main section of ContactUs-skeleton in desktop-media.

--- src/components/ContactUsSkeleton/ContactUsSkeleton.jsx
+++ src/components/ContactUsSkeleton/ContactUsSkeleton.jsx
@@ -11,36 +11,34 @@ const ContactUsSkeleton = () => {
   return (
     // <motion.div className={styles.section} style={{ opacity: pulseOpacity }}>
     <div className={styles.section}>
-      <div className={styles.container}>
-        <div className={styles.innerSection}>
-          <div className={styles.back} />
-          <div className={styles.innerContainer}>
-            <div className={styles.title} />
-            <div className={styles.form}>
-              <div className={styles.input} />
-              <div className={styles.input} />
-              <div className={styles.textArea} />
-              <div className={styles.button} />
-              <div className={styles.button} />
-            </div>
+      <div className={styles.innerSection}>
+        <div className={styles.back} />
+        <div className={styles.innerContainer}>
+          <div className={styles.title} />
+          <div className={styles.form}>
+            <div className={styles.input} />
+            <div className={styles.input} />
+            <div className={styles.textArea} />
+            <div className={styles.button} />
+            <div className={styles.button} />
           </div>
         </div>
-        <div className={styles.innerSection}>
-          <div className={styles.back} />
-          <div className={styles.innerContainer}>
-            <div className={styles.content}>
-              <div className={styles.title} />
-              <div className={styles.list}>
-                <div className={styles.item} />
-                <div className={styles.item} />
-                <div className={styles.item} />
-                <div className={styles.item} />
-              </div>
+      </div>
+      <div className={styles.innerSection}>
+        <div className={styles.back} />
+        <div className={styles.innerContainer}>
+          <div className={styles.content}>
+            <div className={styles.title} />
+            <div className={styles.list}>
+              <div className={styles.item} />
+              <div className={styles.item} />
+              <div className={styles.item} />
+              <div className={styles.item} />
             </div>
-            <div className={styles.aside}>
-              <div className={styles.subTitle} />
-              <div className={styles.text} />
-            </div>
+          </div>
+          <div className={styles.aside}>
+            <div className={styles.subTitle} />
+            <div className={styles.text} />
           </div>
         </div>
       </div>
