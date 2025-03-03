fc958a0027a42cf64219f62651f09a860ba04495
basesnel <basesnel@gmail.com>
2025-03-03 16:17:12 +0200

    Apply Array in the contactUs-skeleton.

--- src/components/ContactUsSkeleton/ContactUsSkeleton.jsx
+++ src/components/ContactUsSkeleton/ContactUsSkeleton.jsx
@@ -30,10 +30,13 @@ const ContactUsSkeleton = () => {
           <div className={styles.content}>
             <div className={styles.title} />
             <div className={styles.list}>
+              {[...Array(4)].map((_, i) => (
+                <div key={i} className={styles.item} />
+              ))}
+              {/* <div className={styles.item} />
               <div className={styles.item} />
               <div className={styles.item} />
-              <div className={styles.item} />
-              <div className={styles.item} />
+              <div className={styles.item} /> */}
             </div>
           </div>
           <div className={styles.aside}>
