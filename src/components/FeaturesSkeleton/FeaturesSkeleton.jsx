03051b843cc506a67e8301519d4f9f6186950ad9
basesnel <basesnel@gmail.com>
2025-03-03 16:55:46 +0200

    Apply gradient in the Features-skeleton.

--- src/components/FeaturesSkeleton/FeaturesSkeleton.jsx
+++ src/components/FeaturesSkeleton/FeaturesSkeleton.jsx
@@ -13,7 +13,7 @@ const FeaturesSkeleton = () => {
     <div className={styles.section}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <div className={styles.subtitle} />
+          <div className={styles.subTitle} />
           {[...Array(3)].map((_, i) => (
             <div key={i} className={styles.title} />
           ))}
