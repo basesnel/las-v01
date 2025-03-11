e0dba7a82f8b4f33602e5528698b7b1fbf40fce1
basesnel <basesnel@gmail.com>
2025-03-11 09:22:56 +0200

    Added IntroSkeleton into Skeleton.

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -1,5 +1,6 @@
 import HeaderSkeleton from "./HeaderSkeleton/HeaderSkeleton";
 import BannerSkeleton from "./BannerSkeleton/BannerSkeleton";
+import IntroSkeleton from "./IntroSkeleton/IntroSkeleton";
 
 import styles from "./styles.module.css";
 
@@ -8,6 +9,7 @@ const Skeleton = () => {
     <div className={styles.skeleton}>
       <HeaderSkeleton />
       <BannerSkeleton />
+      <IntroSkeleton />
     </div>
   );
 };
