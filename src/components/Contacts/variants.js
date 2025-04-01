20f2c471ba40d2603ca2688b060cc13c6b8fe4ed
basesnel <basesnel@gmail.com>
2025-04-01 10:29:27 +0300

    Fixed icon position for items of contact list.

--- src/components/Contacts/variants.js
+++ src/components/Contacts/variants.js
@@ -20,10 +20,12 @@ const contactIcon = {
   initial: {
     scale: 1,
     rotate: 0,
+    y: 3,
   },
   hover: {
     scale: [1, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2],
     rotate: [0, 0, -13, 8, -5, 3, -2, 1, 0],
+    y: 3,
     transition: {
       delay: 0.5,
       duration: 0.5,
